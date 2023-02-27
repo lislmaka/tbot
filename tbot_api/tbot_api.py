7214b0bb111aa3282e4e09bd55240ef92e68a9f3
LS <lislmaka@gmail.com>
2023-02-27 17:09:31 +0300

    fix

--- tbot_api/tbot_api.py
+++ tbot_api/tbot_api.py
@@ -163,11 +163,11 @@ class tbot_api():
         Проверяем есть такая команда
         """
         command, *params = self.get_text().lower().split(' ')
-        try:
-            if command in self.command_dict:
-                self.command = self.command_dict.get(command)
-                self.command_params = ' '.join(params).strip()
-        except:
+
+        if command in self.command_dict:
+            self.command = self.command_dict.get(command)
+            self.command_params = ' '.join(params).strip()
+        else:
             raise tbot_exception.TbotExceptionInvalidCommand(command=command)                
         # else:
         #     self.response_message = 'Нет такой команды *{}*'.format(
