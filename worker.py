188bb987ae7c344b8d46f6ae2a8aab6132a96a05
LS <lislmaka@gmail.com>
2023-02-27 15:20:40 +0300

    fix

--- worker.py
+++ worker.py
@@ -24,7 +24,7 @@ class worker():
 
         self.commands = commands.commands(tbot_api=self.tbot_api)
 
-        self.check_user_info()
+        # self.check_user_info()
 
     # --------------------------------------------------------------------------- #
     #
@@ -61,7 +61,6 @@ class worker():
         mycursor.close()
         mydb.close()
 
-        # self.log_me(myresult)
         if not myresult:
             self.services.logs(msg=myresult, file_name="worker")
             self.tbot_api.send_message(
@@ -150,136 +149,4 @@ class worker():
     def echo_server(self):
         """ Send text response """
         self.commands.echo_server()
-        # self.response_message = 'Ваш текст *{}*. Перевернутый текст *{}*'.format(
-        #     self.tbot_api.get_text(), self.tbot_api.get_text()[::-1])
 
-        # self.tbot_api.send_message(self.response_message)
-
-    # # --------------------------------------------------------------------------- #
-    # #
-    # # --------------------------------------------------------------------------- #
-    # def command_test(self, params):
-    #     """
-    #     """
-    #     self.tbot_api.send_message("command_test")
-
-    # --------------------------------------------------------------------------- #
-    #
-    # --------------------------------------------------------------------------- #
-    # def command_start(self, params):
-    #     """
-    #     """
-    #     # {"inline_keyboard": [[{"text":"Visit Unofficed", "url": "http://unofficed.com"}]]}
-
-    #     reply_markup = {
-    #         "inline_keyboard": [
-    #             [
-    #                 {"text": "Visit Unofficed", "callback_data": "d34"},
-    #                 {"text": "Visit Unofficed 3", "url": "http://unofficed.com"}
-    #             ],
-    #             [
-    #                 {"text": "Visit Unofficed 1", "url": "http://unofficed.com"}
-    #             ],
-    #         ]
-    #     }
-
-    #     self.tbot_api.set_reply_markup(reply_markup)
-    #     self.tbot_api.send_message(
-    #         "Выберите интересующий вас вариант дальнейшего развития событий")
-
-    # # --------------------------------------------------------------------------- #
-    # #
-    # # --------------------------------------------------------------------------- #
-    # def command_start(self, params):
-    #     """
-    #     """
-    #     word = ["С _ К _ _ А _ _ М _"]
-    #     hint = ["Хочу подсказку"]
-
-    #     reply_markup = {
-    #         "keyboard": [],
-    #         "resize_keyboard": True
-    #     }
-
-    #     reply_markup["keyboard"].append(word)
-    #     reply_markup["keyboard"].append(hint)
-
-    #     self.tbot_api.set_reply_markup(reply_markup)
-    #     self.tbot_api.send_message("command start")
-
-    # # --------------------------------------------------------------------------- #
-    # #
-    # # --------------------------------------------------------------------------- #
-    # def command_keyboard_show(self, params):
-    #     """
-    #     """
-    #     commands = [
-    #         ["Представьтесь пожалуйста (ФИО)",
-    #          "Ваша основная специализация"],
-    #         ["Укажите ваш номер телефона", "Укажите ваш e-mail", "Из какого вы города"]]
-
-    #     reply_markup = {
-    #         "keyboard": [],
-    #         "resize_keyboard": True
-    #     }
-
-    #     for command in commands:
-    #         reply_markup["keyboard"].append(command)
-
-    #     self.tbot_api.set_reply_markup(reply_markup)
-    #     self.tbot_api.send_message("command start")
-
-    # # --------------------------------------------------------------------------- #
-    # #
-    # # --------------------------------------------------------------------------- #
-    # def command_keyboard_hide(self, params):
-    #     """
-    #     """
-    #     reply_markup = {
-    #         "remove_keyboard": True
-    #     }
-    #     self.tbot_api.set_reply_markup(reply_markup)
-    #     self.tbot_api.send_message("command stop")
-
-    # # --------------------------------------------------------------------------- #
-    # #
-    # # --------------------------------------------------------------------------- #
-    # def command_stop(self, params):
-    #     """
-    #     """
-    #     reply_markup = {
-    #         "remove_keyboard": True
-    #     }
-    #     self.tbot_api.set_reply_markup(reply_markup)
-    #     self.tbot_api.send_message("command stop")
-
-    # # --------------------------------------------------------------------------- #
-    # #
-    # # --------------------------------------------------------------------------- #
-    # def command_password(self, params):
-    #     """
-    #     """
-    #     password_len = 10
-    #     if params:
-    #         try:
-    #             password_len = int(params)
-    #         except:
-    #             pass
-
-    #     alphabet = string.ascii_letters + string.digits
-    #     password = ''.join(secrets.choice(alphabet)
-    #                        for i in range(password_len))
-    #     self.response_message = password
-    #     self.tbot_api.send_message(self.response_message)
-
-    # # --------------------------------------------------------------------------- #
-    # #
-    # # --------------------------------------------------------------------------- #
-    # def command_time(self, params):
-    #     """
-    #     """
-    #     now = datetime.now()
-    #     current_time = now.strftime("%H:%M:%S")
-
-    #     self.response_message = 'Текущее время - *'+current_time+'*'
-    #     self.tbot_api.send_message(self.response_message)
